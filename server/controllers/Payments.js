8973366951d613219d31a9d1e61baf5914a81374
rakesh1605 <135423514+rakesh1605@users.noreply.github.com>
2025-08-04 18:50:20 +0530

    Update Payments.js

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -100,7 +100,7 @@ exports.verifySignature=async(req,res)=>{
   }
 
   let body=razorpay_order_id + "|" + razorpay_payment_id;
-  console.log("Razorpay Secret:", process.env.RAZORPAY_SECRET);
+  console.log("Razorpay Secret:", process.env.Razorpay_SECRET);
  const expectedSignature=crypto.createHmac("sha256",process.env.RAZORPAY_SECRET).update(body.toString()).digest("hex");
   if(expectedSignature===razorpay_signature){
     //enroll the student
