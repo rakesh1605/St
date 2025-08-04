e872cd5b8187f0a1899fc0e477e0e6eb477b9382
rakesh1605 <135423514+rakesh1605@users.noreply.github.com>
2025-08-04 20:31:50 +0530

    Update Razorpay.js

--- server/config/Razorpay.js
+++ server/config/Razorpay.js
@@ -1,6 +1,6 @@
 const Razorpay = require("razorpay");
 
 const instance = new Razorpay({
-  key_id: process.env.Razorpay_KEY.trim(),     // Trim removes \n or spaces
-  key_secret: process.env.Razorpay_SECRET.trim()
+  key_id: process.env.Razorpay_KEY,     // Trim removes \n or spaces
+  key_secret: process.env.Razorpay_SECRET
 });
